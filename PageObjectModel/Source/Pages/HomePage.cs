d7dbb185190c4032390e74161be2d5045f76cef5
mdmostafamunna <mdmostafamunna.sqa@gmail.com>
2023-05-03 15:05:03 +0600

    Add project files.

---
+++ PageObjectModel/Source/Pages/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class HomePage
+    {
+
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Id, Using = "twotabsearchtextbox")]
+        private IWebElement _searchtxtBox;
+
+        [FindsBy(How = How.Id, Using = "nav-search-submit-button")]
+        private IWebElement _searchbtn;
+
+        public HomePage(IWebDriver driver) 
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Search(string searchText)
+        {
+
+            _searchtxtBox.SendKeys(searchText);
+            _searchbtn.Click();
+
+        }
+    }
+}
---
+++ PageObjectModel/Test/HomeTest.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjectModel.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace PageObjectModel.Test
+{
+    public class HomeTest
+    {
+
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitScript()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+
+        }
+
+
+
+        [Test]
+        public void SearchBook()
+        {
+            HomePage hp = new HomePage(_driver);
+            _driver.Navigate().GoToUrl("https://www.amazon.com/");
+            hp.Search("Selenium book");
+            Assert.True(_driver.Title.Contains("Selenium book"));
+        }
+
+        [TearDown]
+        public void Clearup()
+        {
+            _driver.Quit();
+        }
+    }
+}
